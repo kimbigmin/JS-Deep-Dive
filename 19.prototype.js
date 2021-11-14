c82a0e2f0ced7d98e7f88c59778096a7675f338c
kimmingyu <76110448+kimbigmin@users.noreply.github.com>
2021-11-14 23:29:06 +0900

    프로토타입 예시코드 추가

--- 19.prototype.js
+++ 19.prototype.js
@@ -150,6 +150,22 @@ console.log(me.constructor === Person); // true
 
 // 19.4 리터럴 표기법으로 생성된 객체의 생성자 함수와 프로토타입 @@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
+// obj 객체를 생성한 생성자 함수는 Object다.
+const obj = new Object();
+console.log(obj.constructor === Object); // true
+
+// add 함수 객체를 생성한 생성자 함수는 Function 이다.
+const add = new Function('a', 'b', 'return a + b');
+console.log(add.constructor === Function); // true
+
+// 생성자 함수
+function Person(name) {
+    this.name = name;
+}
+
+//me 객체를 생성한 생성자 함수는 Person이다.
+const me = new Person('Lee');
+console.log(me.constructor === Person); // true
 
 
 
